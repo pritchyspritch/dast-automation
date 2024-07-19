12d04d96a3d538ae6ebcf262f18ec6de41449c24
pritchyspritch <47423802+pritchyspritch@users.noreply.github.com>
2024-07-19 17:24:50 +0100

    Make the script work as a headless browser

--- selenium_login.py
+++ selenium_login.py
@@ -1,16 +1,15 @@
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 import time
 import os
-
-
-def click_button(element):
-    action = ActionChains(driver)
-    action.click(on_element=element)
-    action.perform()
-    time.sleep(3)
+import subprocess
+import yaml
+import platform
+import json
 
 
 def set_secret(name, value):
@@ -30,13 +29,62 @@ def set_secret(name, value):
             secrets_yaml.write(secrets)
 
 
-driver = webdriver.Firefox()
+def run_gospider():
+    with open("secrets.yaml", "r") as secrets_yaml:
+        yaml_secrets = yaml.safe_load(secrets_yaml)
+        cookies_list = yaml_secrets["static"][0]["cookies"]
+
+    for cookie in cookies_list:
+        if cookie["key"] == "session":
+            session = cookie["value"]
+        elif cookie["key"] == "_csrf":
+            csrf = cookie["value"]
+
+    gospider = subprocess.run(f"gospider --site https://pp-services.signin.education.gov.uk --cookie 'session={session}; _csrf={csrf}' --blacklist '(\.(js|png|ico|css|1)|session\/end|signout)' --output ./ -vv --debug --json -d 4", shell=True)
+
+
+def get_spider_urls():
+    with open("pp-services_signin_education_gov_uk", "r") as urls_file:
+        urls = urls_file.readlines()
+    
+    urls_list = []
+    for url in urls:
+        url_json = json.loads(url)
+
+        match url_json["type"]:
+            case "subdomain":
+                if url_json["output"].endswith("signin.education.gov.uk"):
+                    urls_list.append(f"https://{url_json['output']}")
+            case "url":
+                urls_list.append(url_json["output"])
+            case "form":
+                if url_json["output"].endswith(".ico"):
+                    continue
+                elif url_json["output"].endswith(".png"):
+                    continue
+                elif "css" in url_json["output"]:
+                    continue
+                elif "signin.education.gov.uk" not in url_json["output"]:
+                    continue
+                else:
+                    urls_list.append(url_json['output'])
+            case "javascript":
+                continue
+    
+    with open("urls.txt", "w") as output_urls:
+        for url in list(set(urls_list)):
+            output_urls.write(f"{url}\n")
+
+
+options = webdriver.FirefoxOptions()
+options.add_argument("-headless")
+driver = webdriver.Firefox(options=options)
+
 
 driver.get("https://pp-services.signin.education.gov.uk/")
 
-start_button = driver.find_element(By.LINK_TEXT, "Start now")
+WebDriverWait(driver, 1000).until(EC.element_to_be_clickable((By.LINK_TEXT, "Start now"))).click()
 
-click_button(start_button)
 
 username = os.getenv("USER")
 password = os.getenv("PASSWORD")
@@ -47,10 +95,10 @@ username_box.send_keys(username)
 password_box = driver.find_element(By.ID, "password")
 password_box.send_keys(password)
 
-login_button = driver.find_element(By.XPATH, "//button[@class='govuk-button']")
+password_box.send_keys(Keys.ENTER)
 
-click_button(login_button)
 
+time.sleep(3)
 print(driver.title)
 
 
@@ -60,12 +108,20 @@ if driver.title == "Access DfE services":
 
         set_secret(cookie['name'], cookie['value'])
 
+    driver.quit()
+    
+    # Run the spider and nuclei from python if local - do in actions steps in github
+    if platform.system() == "Darwin":
+        run_gospider()
+        get_spider_urls()
 
-# TODO run gospider --site https://pp-services.signin.education.gov.uk --cookie "session=; _csrf=" --blacklist "(\.(js|png|ico|css|1)|session\/end|signout)" --output ./ -vv --debug --json -d 4
-# TODO parse json / get URLs
-# TODO run nuclei -l URLS.txt -sf secrets.yaml
+        nuclei = subprocess.run("nuclei -l urls.txt -sf secrets.yaml -json-export nuclei_output.json", shell=True)
 
-driver.quit()
+else:
+    driver.quit()
+
+
+
 
 
 
